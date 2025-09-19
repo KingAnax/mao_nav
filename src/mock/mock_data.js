d6ae94faa9fa0dff192093da6165cd84846d78c9
yulumao <102864013+KingAnax@users.noreply.github.com>
2025-09-19 23:01:44 +0800

    chore: 更新导航数据 - 2025/9/19 23:01:43

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -12,20 +12,6 @@ export const mockData = {
           "url": "https://nanobananafree.ai/",
           "description": "free图片生成",
           "icon": "https://nanobananafree.ai/favicon.ico"
-        },
-        {
-          "id": "site-1758122046828",
-          "name": "豆包",
-          "url": "https://www.doubao.com/",
-          "description": "豆包AI",
-          "icon": "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png"
-        },
-        {
-          "id": "site-1758122098819",
-          "name": "kimi",
-          "url": "https://www.kimi.com/",
-          "description": "kimi AI",
-          "icon": "https://statics.moonshot.cn/kimi-web-seo/favicon.ico"
         }
       ]
     },
